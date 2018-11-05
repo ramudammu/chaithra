fe8d7dd9ac05a7e4a70088b989b6123e92d6d5ac
Ramu Dammu <ramudammu@gmail.com>
2018-11-05 15:11:27 -0500

    Revert "revert22"
    This reverts commit 6f27b7d457fdce589ed17a0618198105e330abc2.

--- hello.py
+++ hello.py
@@ -1,10 +1,4 @@
 print("hello world")
 print(" I need a girl like you")
 print("Love me like you do").
-jjjjjjjjjjjjjjjjjjjjjjjjjjjjj
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
-kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
-kkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
-mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm
-nnnnnnnnnnnnnnnnnnnnnnnnnnnnn
 
